abcfd26fac6a82dfef933e563d735b3e074476f0
Josh Walawender <jmwalawender@gmail.com>
2019-06-15 19:54:11 -1000

    Continue to draft observatory

--- pypaca/observatory.py
+++ pypaca/observatory.py
@@ -38,19 +38,51 @@ class Sequence(object):
     
     Properties
     images -- List of ImageConf objects
-    ditherparams -- 
+    dithersize -- Dither size (0 for no dither)
     """
-    
-
+    def __init__(self, images=None, dithersize=0):
+        self.images = images
+        self.dithersize = dithersize
 
 
 ##-------------------------------------------------------------------------
 ## Observatory
 ##-------------------------------------------------------------------------
 class observatory(object):
-    def connect(self):
-        pass
-    
+    def connect_camera1(self, *args, **kwargs):
+        """Camera1 is assumed to the main imaging camera."""
+        self.camera1 = devices.Camera(*args, **kwargs)
+
+    def connect_filterwheel1(self, *args, **kwargs):
+        """FilterWheel1 is assumed to associated with the main imaging camera.
+        """
+        self.filterwheel1 = devices.FilterWheel(*args, **kwargs)
+
+    def connect_focuser1(self, *args, **kwargs):
+        """Focuser1 is assumed to the focuser for the main imaging camera."""
+        self.focuser1 = devices.Focuser(*args, **kwargs)
+
+    def connect_camera2(self, *args, **kwargs):
+        """Camera2 is assumed to the guide camera."""
+        self.camera2 = devices.Camera(*args, **kwargs)
+
+    def connect_filterwheel2(self, *args, **kwargs):
+        """FilterWheel2 is assumed to associated with the guide camera."""
+        self.filterwheel1 = devices.FilterWheel(*args, **kwargs)
+
+    def connect_focuser2(self, *args, **kwargs):
+        """Focuser2 is assumed to the focuser for the guide camera."""
+        self.focuser2 = devices.Focuser(*args, **kwargs)
+
+    def connect_telescope(self, *args, **kwargs):
+        """Telescope (mount) on which the other devices ride."""
+        self.telescope = devices.Telescope(*args, **kwargs)
+
+    def load_config(self, file):
+        with open('test.yaml', 'r') as f:
+            contents = f.read()
+            self.config = yaml.safe_load(contents)
+
     def acquire_image(self, conf):
         """Acquire a single image with the given characteristics.
         """
