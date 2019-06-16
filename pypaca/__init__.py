1090c1135d0c84412835c11238d451d1835045d6
Josh Walawender <jmwalawender@gmail.com>
2019-06-15 18:13:40 -1000

    rearrange items

--- pypaca/__init__.py
+++ pypaca/__init__.py
@@ -1,2 +1,32 @@
-print(f'Invoking __init__.py for {__name__}')
-from .devices import *
+verbose = True
+
+##-------------------------------------------------------------------------
+## Create logger object
+##-------------------------------------------------------------------------
+import logging
+log = logging.getLogger('MyLogger')
+log.setLevel(logging.DEBUG)
+## Set up console output
+LogConsoleHandler = logging.StreamHandler()
+if verbose is True:
+    LogConsoleHandler.setLevel(logging.DEBUG)
+else:
+    LogConsoleHandler.setLevel(logging.INFO)
+LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s')
+LogConsoleHandler.setFormatter(LogFormat)
+log.addHandler(LogConsoleHandler)
+## Set up file output
+# LogFileName = None
+# LogFileHandler = logging.FileHandler(LogFileName)
+# LogFileHandler.setLevel(logging.DEBUG)
+# LogFileHandler.setFormatter(LogFormat)
+# log.addHandler(LogFileHandler)
+
+
+##-------------------------------------------------------------------------
+## AlpacaError
+##-------------------------------------------------------------------------
+class AlpacaError(Exception):
+    pass
+
+from . import devices
--- pypaca/devices.py
+++ pypaca/devices.py
@@ -1,7 +1,4 @@
 #!/usr/env/python
-
-import argparse
-import logging
 import random
 import time
 
@@ -10,50 +7,7 @@ import json
 
 import numpy as np
 
-##-------------------------------------------------------------------------
-## Parse Command Line Arguments
-##-------------------------------------------------------------------------
-## create a parser object for understanding command-line arguments
-p = argparse.ArgumentParser(description='''
-''')
-## add flags
-p.add_argument("-v", "--verbose", dest="verbose",
-    default=False, action="store_true",
-    help="Be verbose! (default = False)")
-## add options
-# p.add_argument("--input", dest="input", type=str,
-#     help="The input.")
-## add arguments
-# p.add_argument('argument', type=int,
-#                help="A single argument")
-# p.add_argument('allothers', nargs='*',
-#                help="All other arguments")
-args = p.parse_args()
-
-
-##-------------------------------------------------------------------------
-## Create logger object
-##-------------------------------------------------------------------------
-log = logging.getLogger('MyLogger')
-log.setLevel(logging.DEBUG)
-## Set up console output
-LogConsoleHandler = logging.StreamHandler()
-if args.verbose is True:
-    LogConsoleHandler.setLevel(logging.DEBUG)
-else:
-    LogConsoleHandler.setLevel(logging.INFO)
-LogFormat = logging.Formatter('%(asctime)s %(levelname)8s: %(message)s')
-LogConsoleHandler.setFormatter(LogFormat)
-log.addHandler(LogConsoleHandler)
-## Set up file output
-# LogFileName = None
-# LogFileHandler = logging.FileHandler(LogFileName)
-# LogFileHandler.setLevel(logging.DEBUG)
-# LogFileHandler.setFormatter(LogFormat)
-# log.addHandler(LogFileHandler)
-
-class AlpacaError(Exception):
-    pass
+from . import log
 
 ##-------------------------------------------------------------------------
 ## Abstract Alpaca Device
